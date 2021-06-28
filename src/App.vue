9802344d2a8b24cb9acb394507c08153491cb9b0
Leticia <leticiancalves1@gmail.com>
2021-06-28 14:37:43 -0300

    criando segundo menu

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <Navbar />
+    <Menu />
    </div> 
 </template>
 
 <script>
-import Navbar from './paginas/navbar.vue'
+import Navbar from './paginas/navbar.vue';
+import Menu from './paginas/menu-vertical.vue'
 export default {
   name: 'app',
 components: {
   Navbar, 
+  Menu, 
 }
 }
 </script>
---
+++ src/paginas/menu-vertical.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <!-- CSS only -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+     <div class="row">
+        <div class="col-md-4">
+    <nav class="menuVertical" >
+        <ul>
+            <li><i class="fas fa-home"></i><a href="#">Início</a></li>
+            <li><i class="fas fa-users"></i><a href="#">Escolas</a></li>
+            <li><i class="far fa-user"></i><a href="#">Usuários</a></li>
+            <li><i class="fas fa-graduation-cap"></i><a href="#contato">Diplomas</a></li>
+        </ul>
+    </nav>
+</div>
+</div>
+        </div> 
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.menuVertical{
+    height: 919px;
+    width: 207px;
+    padding-top: 20px;
+    background-color: #F9F9F9;
+
+}
+ul{
+    list-style: none;
+}
+
+ul li{
+    margin: 40px 0px;
+    width: 158px;
+    height: 20px;
+     font-family:'Manrope', sans-serif;
+
+}
+a{
+    padding-left: 10px;
+    color: #989898 ;
+    text-decoration: none;
+}
+</style>
