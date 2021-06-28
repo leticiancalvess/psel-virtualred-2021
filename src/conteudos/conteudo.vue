bca7d833ab95ee5c55bbbeae4c9ee9cbdf5a45c2
Leticia <leticiancalves1@gmail.com>
2021-06-28 17:07:53 -0300

    criação name data att

--- src/conteudos/conteudo.vue
+++ src/conteudos/conteudo.vue
@@ -3,8 +3,8 @@
          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
               
-     <div class="row">
-        <div class="col-md-2">
+     <div class="row"> <!-- inicio row -->
+        <div class="col-md-2"> <!-- inicio menu-vertical -->
     <nav class="menuVertical" >
         <ul>
             <li><i class="fas fa-home"></i><a href="#">Início</a></li>
@@ -13,33 +13,42 @@
             <li><i class="fas fa-graduation-cap"></i><a href="#contato">Diplomas</a></li>
         </ul>
     </nav>
-</div>
+</div>  <!-- fim menu-vertical -->
 
-        <div class="col-md-6 template"> <!-- inicio template -->
-        <h1>Templates de diplomas cadastrados</h1>
+        <div class="col-md-5 template"> <!-- inicio template -->
+        Templates de diplomas cadastrados
         
-        <div class="col-md-12"> <!--filtros e ordenar -->
-            <div id="contentTop">
+        
+        <div class="col-md-6"><!--filtros e ordenar -->
+            <div id="contentTop"> 
             <div id="filtros"><i class="fas fa-filter"></i>Filtros</div>
             <div id="traco"></div>
             <div id="ordenar"> Ordenar por</div>
             </div>
-            </div> <!--fim filtros e ordenar -->
+             </div> <!--fim filtros e ordenar -->
+            
+          
             
             </div> <!-- fim inicio template -->
              
-            <div class="col-md-4 cadastrar"> <!-- inicio cadastrar -->
-                Cadastrar
+            <div class="col-md-5 d"> <!-- inicio cadastrar -->
+               <div id="cadastrar"> Cadastrar </div>
+                <div class="col-md-4 DataNameAtt"> <!-- inicio data name att -->
+                <div id="data">
+                    Data
+                </div>
+                <div id="nome">
+                    Nome
+                </div>
+                <div id="att">
+                    Atualização
+                </div>
+            </div> <!-- fim data name att -->
                 </div><!-- fim cadastrar -->
         
-        </div>
+        </div> <!-- fim row -->
        
         </div>
-            
-       
-        </div>
-        </div>
-</div>
 </template>
 
 <script>
@@ -72,9 +81,12 @@ a{
 }
 .template{
     margin-top: 40px;
+    font-size: 28px;
+    font-weight: bold;
+    font-family: 'Rubik',sans-serif;
     
 }
-.cadastrar{
+#cadastrar{
     background-color: black;
     color: white;
     width: 120px;
@@ -85,7 +97,7 @@ a{
     margin-left: 200px;
     border-radius: 15px;
     margin-top: 40px;
-    float: right;
+    
 }
 
 #filtros{
@@ -93,10 +105,12 @@ a{
     float: left;
     margin-right: 20px;
     color: #989898;
+    font-size: 17px;
 }
 #ordenar{
     float:left;
     color: #989898;
+    font-size: 17px;
 }
 #traco{
     border: 0.1px solid #989898;;
@@ -113,4 +127,39 @@ a{
     margin-top: 50px;
 }
 
+
+.DataNameAtt{
+    margin-top: 60px;
+    display: inline-block;
+    width: 400px
+   
+    
+}
+#data, #nome, #att{
+    float: left;
+    padding-right: 10px;
+    font-family: 'Roboto';
+    font-size: 14px;
+    width: 100px;
+    text-align: center;
+    color: white;
+    height: 40px;
+    padding-top: 10px;
+   margin-left: 2px;
+}
+#data{
+    background-color: black;
+     border-radius: 10px 0 0 10px;
+    
+}
+#nome{
+    border-radius: none;
+}
+#att{
+    border-radius: 0 10px 10px 0;
+}
+#nome, #att{
+    background-color: #B28DD6;
+
+}
 </style>
