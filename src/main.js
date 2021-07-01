8601449a70966893a5211355da0a393fb36010aa
Leticia <leticiancalves1@gmail.com>
2021-07-01 12:21:39 -0300

    responsividade navbar

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './rotas/router.js'
+import {BootstrapVue, IconsPlugin} from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 new Vue({
   el: '#app',
   router,
--- src/paginas/navbar.vue
+++ src/paginas/navbar.vue
@@ -1,25 +1,27 @@
 <template>
-    <div>
-        <!-- CSS only -->
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <!-- fim links -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-light"> <!-- inicio navbar -->
-            <a class="navbar-brand logo" href="#"></a> 
-            <form class="form-inline searchFuntionality search"> <!-- inicio buscar -->
-                <i class="fas fa-search"></i>
-            <input class="form-control" type="search"  placeholder="Buscar Funcionalidade" aria-label="Search"> 
-            </form><!-- fim buscar -->
-    <div class="superAdmin"> <!-- inicio avatar e super admin -->
-    <div class="avatar"></div>
-    <div id="text">
-       <div class="topText">Super Admin</div><br>
-        <div class="bottomText">Super Admin</div>
-        </div>
-    </div><!-- fim avatar e super admin -->
-   
-</nav><!-- fim navbar -->
-</div>
+     <b-container-fluid>
+         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
+      <b-navbar toggleable="lg" class="nav">
+        <b-navbar-brand class="logo" href="#"></b-navbar-brand>
+
+        <b-nav-form class="searchFuntionality search">
+            <i class="fas fa-search"></i>
+          <b-form-input size="sm" class="mr-sm-2" placeholder="Buscar Funcionalidade"></b-form-input>
+        </b-nav-form>
+
+       
+          <b-navbar-nav class="ms-auto">
+              <div class="superAdmin">
+             <div class="avatar"></div>
+             <div id="textTop">Super Admin</div>
+             <br>
+              <div id="textBottom">Super Admin</div>
+             </div>
+          </b-navbar-nav>
+        
+      </b-navbar>
+
+     </b-container-fluid>
 </template>
 
 <script>
@@ -34,50 +36,23 @@
     border-radius: 50%;
     background-color: white;
 }
-nav{
+.nav{
     height: 101px;
-    background-color: #B28DD6 !important; 
+    background-color: #B28DD6; 
 }
+
 .searchFuntionality{
     width: 476px ;
 }
-.avatar{
-    width: 40px;
-    height: 40px;
-    background-color: #C4C4C4;
-    border-radius: 10px;
-    float: left;
-    margin-right: 10px;
-  
-}
-.superAdmin{
-    padding-left: 300px;
-    display: inline;
-    width: 500px;
-}
-#text{
-    float: left;
-  
-}
-.topText{
-    font-size: 16px;
-    font-weight: bold;
-    font-family: 'Roboto', sans-serif
-}
-.bottomText{
-    margin-top: -20px;
-    font-family: 'Rubik', sans-serif;
-    font-size: 10px;
-    color: white;
-}
+
 .search{
     position: relative;
 }
 
 .search i {
     position: absolute;
-    padding: 10px;
-    color: #989898;
+     padding: 6px 12px;
+     color: #989898;
    
 }
 .search input.form-control {
@@ -85,32 +60,163 @@ nav{
     color: #989898;
 }
 
-@media (min-width: 711px) and (max-width: 990px){
-    .logo{
-    width: 250px;
+.avatar{
+    width: 40px;
+    height: 40px;
+    background-color: #C4C4C4;
+    border-radius: 10px;
+    margin-right: 10px;
+    flex-direction: start;
+    float: left;
+    display: inline;
+   
+  
 }
-nav{
-    height: 121px;
-    background-color: #B28DD6 !important; 
-}
-
-    .searchFuntionality{
+.superAdmin{
+    padding-top: -100px !important;
     width: 300px;
-    margin-left: -30px;
+    
+}
+#textTop{
+    font-size: 16px;
+    font-weight: bold;
+    font-family: 'Roboto', sans-serif;
+    float: left;
+   
+}
+#textBottom{
+    font-family: 'Rubik', sans-serif;
+    font-size: 10px;
+    color: white;
+    
 }
 
+@media only screen and (max-width: 499px){
+.logo{
+    width: 150px;
 }
-/* Small devices (portrait tablets and large phones, 600px and up) */
-@media only screen and (min-width: 600px) {}
+.searchFuntionality{
+    width: 250px ;
+}
+.nav{
+    height: 150px;
+}
+.avatar{
+    flex-direction: end;
+    float: left;
+    display: inline;
+   
+  
+}
+.superAdmin{
+    width: 150px;
+    top: 0%;
+    right: 0;
+    left: 55%;
+}
+#textTop{
+    font-size: 12px;
+    font-weight: bold;
+    font-family: 'Roboto', sans-serif;
+    float: left;
+   
+}
+#textBottom{
+    font-family: 'Rubik', sans-serif;
+    font-size: 10px;
+    color: white;
+    margin-top: -5px;
+    
+}
+}
+@media only screen and (max-width: 800px){
+.logo{
+    width: 130px;
+}
+.searchFuntionality{
+    width: 320px ;
+}
+.nav{
+    height: 150px;
+}
+.avatar{
+    flex-direction: end;
+    float: left;
+    display: inline;
+   
+  
+}
+.superAdmin{
+    width: 150px;
+    top: 0 !important;
+    right: 0;
+    left: 75%;
+    position: fixed;
 
-/* Medium devices (landscape tablets, 768px and up) */
-@media only screen and (min-width: 768px) {}
-
-/* Large devices (laptops/desktops, 992px and up) */
-@media only screen and (min-width: 992px) {}
-
-/* Extra large devices (large laptops and desktops, 1200px and up) */
-@media only screen and (min-width: 1200px) {}
+}
+#textTop{
+    font-size: 12px;
+    font-weight: bold;
+    font-family: 'Roboto', sans-serif;
+    float: left;
+   
+}
+#textBottom{
+    font-family: 'Rubik', sans-serif;
+    font-size: 10px;
+    color: white;
+    margin-top: -5px;
+    
+}
+}
+@media only screen and (max-width: 1000px){
+.logo{
+    width: 150px;
+}
+.searchFuntionality{
+    width: 400px ;
+}
+.nav{
+    height: 150px;
+}
+.avatar{
+    flex-direction: end;
+    float: left;
+    display: inline;
+   
+  
+}
+.superAdmin{
+    width: 150px;
+    top: 5%;
+    right: 0;
+    left: 75%;
+    position: fixed;
+    
+    
+    
+}
+#textTop{
+    font-size: 12px;
+    font-weight: bold;
+    font-family: 'Roboto', sans-serif;
+    float: left;
+   
+}
+#textBottom{
+    font-family: 'Rubik', sans-serif;
+    font-size: 10px;
+    color: white;
+    margin-top: -5px;
+    
+}
+} 
+@media only screen and (max-width: 1600px){
+    
+.superAdmin{
+    right: 55% !important;
+    
+}
+}
 
 </style>
-
