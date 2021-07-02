fa24640c737565fba32ce16c131257405c21a0cc
Leticia <leticiancalves1@gmail.com>
2021-07-02 18:23:15 -0300

    finalizar segunda pagina

--- src/conteudos/diplomacreate.vue
+++ src/conteudos/diplomacreate.vue
@@ -14,101 +14,124 @@
     <div>
     <div class="sub-title container-fluid"> <!-- inicio container -->
     <div class="row"> <!-- inicio row -->
-        <div class="col-md-8 template"> <!--inicio template -->
+        <div class="col-md-10 template"> <!--inicio template -->
                 {{ msg }}
         </div><!--fim template -->
-    <div class="col-md-4 "><!--inicio salvar -->
-        <div id="salvar"> Salvar</div>
+     <div class="col-md-2 d-flex justify-content-start"><!--inicio cadastro -->
+        <div id="salvar">Salvar
     </div><!--fim salvar -->
     </div><!--fim row -->
     </div> <!-- fim container -->
+     <section class="flex">
+      <div>
     
-   
-    <div class="container-fluid"><!-- inicio container -->
-  <div class="row"><!-- inicio row -->
-<div class="col-md-3"><!--subir imagem -->
- 
 <button  v-on:click="isHidden = !isHidden" id="select-image"><input type="file" id="imageInput" accept = "image/*"><div id="postImage">Subir Imagem<i class="fas fa-images iconPostImage"></i> </div></button>
-  <div class="traco2"></div>
-  </div><!-- fim subir imagem -->
-
-  <div class="col-md-3 shapes"><!--formas -->
+        
+      </div>
+      <div>
         Formas:
-  <i v-on:click="isHiddenFormas = !isHiddenFormas" class="fas fa-star icon"></i>
-  <div class="traco"></div>
-  </div><!--fim formas -->
-
-  <div class="col-md-3 fields"><!--campos -->
+        <i v-on:click="isHiddenFormas = !isHiddenFormas" class="fas fa-star icon"></i>
+        <div class="traco"></div>
+      </div>
+      <div>
         Campos: 
-  <i class="fas fa-text-height icon"></i>
-  <i class="fas fa-images icon"></i>
-  <div class="traco"></div>
-  </div><!-- fimcampos -->
-
-     <div class="col-md-3 editing"> <!-- frente e verso -->
-     <div id="edit">
-        Editando:
-     </div>
-    
+        <i class="fas fa-text-height icon"></i>
+        <i class="fas fa-images icon"></i>
+      </div>
+       <div >
+           <div id="edit">
+               Editando:
+           </div>
+        
      <div id="front">
          Frente
      </div>
      <div id="verse">
          Verso
         </div>
-         
-      </div><!-- fim frente e verso -->
-    </div> <!-- fim row -->
- </div> <!-- fim container -->
-    <div class="col-md-10 linha"> <!-- inicio linha -->
-      <div class="underTheLine">
-        Diplomas
       </div>
-      </div> <!-- fim linha -->
-<div class="row">
-
-<div class="col-md-10">
-<canvas id= "myCanvas" ></canvas>
-</div>
-<div  class="col-md-2 d-flex boxes">
-    <div  v-if="isHidden" class="col-md-12 boxDimension">
-    <div class="col-md-6 XW">
-        X 112
-        <br><br>
-        W 193
-    </div>
-    <div class="col-md-6 YH">
-        Y 413
-        <br><br>
-        H 60
-    </div>
-    </div>
-    <div v-if="isHiddenFormas" class="col-md-12 stroke" >
+    
+    </section>
+     <div class="col-md-10 linha">
+      <div class="underTheLine">
+          Diploma
+          </div>
+          </div>
+   
+         <section class="grid2">
+      <div class="video">
+          <canvas id= "myCanvas" ></canvas>
+        
+      </div>
+      <div class="sidebar">
+        <div>
+            <div v-if="isHidden" class="col-md-12 boxDimension">
+                <div class="col-md-6 XW">
+                    X 112
+                    <br><br>
+                    W 193
+                </div>
+                <div class="col-md-6 YH">
+                    Y 413
+                    <br><br>
+                    H 60
+                </div>
+                </div>
+                 <div  v-if="isHiddenFormas" class="col-md-12 stroke" >
         <div class="col-md-12">
         <div  class="col-md-10" id="strokeWord">Stroke</div>
         
         <div class="col-md-2" id="plus"><i class="fas fa-plus"></i></div>
+        
+           <div class="container" >
+	<div class="item1" >
+        <div id="caixaColor">
+            <div id="zero"> 000000</div>
+           
         </div>
-        <div class="col-md-8 oi">
-            </div>
-    </div> 
+    </div>
+    
+	<div class="item" >
+        <i class="far fa-eye eye"></i><i class="fas fa-minus minus"></i>
+    </div>
+    </div>
+    <div class="container2" >
+        <div id="bars">
+             <i class="fas fa-bars"></i>
+             3
+        </div>
+          <div id="center">
+             Center <i class="fas fa-chevron-down"></i>
+            
+        </div>
+    
+        </div>
+        <div class="container" >
+	    <div class="item3" >
+            
+                <i class="fas fa-minus  down2" ></i><i class="fas fa-chevron-down down"></i>
+                </div>
+	    <div class="item3" ><i class="fas fa-minus  down2"></i><i class="fas fa-chevron-down down"></i></div>
+	    <div class="item4" ><i class="fas fa-ellipsis-h ellipsis"></i></div>
+        </div>
+	
+	
+
+        </div>
+        </div>
+        </div>
+        </div> 
+
+    </section>
+    
+  </div>
+</div>
+    </div>
+    </div>
 
     
-</div>
-
- </div>
+ </template>
      
- </div>
-</div>
- 
-
-
-
-</div> 
-
-
-</template>
-
 <script>
 import Vue from 'vue';
 import { SidebarPlugin } from '@syncfusion/ej2-vue-navigations';
@@ -121,6 +144,7 @@ export default {
             msg: 'Cadastrar novo template',
             isHidden: false,
             isHiddenFormas: false,
+        
             items: [
                 {
                     label: 'Início',
@@ -142,6 +166,7 @@ export default {
             ]
         }
     },
+    
      mounted() {
         let imgInput = document.getElementById('imageInput');
         imgInput.addEventListener('change', function(e) {
@@ -171,6 +196,23 @@ export default {
 <style scoped>
 
 
+.linha{
+    margin-top: 30px;
+    border-top: 1px dashed rgb(180, 177, 177);
+    position: relative;
+    width: 100%;
+}
+.underTheLine{
+    position: absolute;
+    background-color: black;
+    border-radius: 30px;
+    text-align: center;
+    margin-top: -10px;
+    color: white;
+    width: 90px;
+    margin-left: 10px;
+    text-transform: uppercase;
+}
 .sub-title {
     padding: 10px;
 }
@@ -221,25 +263,51 @@ nav ul li a {
     height: 50px;
     border-radius: 15px;
     padding-top: 10px;
-    margin-left: 200px;
     margin-top: 10px;
     text-align: center;
+    justify-content: center !important;
    }
-
-
-.editing{
-    display: inline;
-    justify-content: center;
-    width: 300px !important;
-    
-}
-#edit {
+   
+.oi{
+   
     float: left;
-    height: 40px;
-    color:#989898;
-    margin-right: 10px;
-    margin-top: 5px;
+} 
+.fields, .shapes, .editing{
+    float: left;
+    border: 1px solid blue;
 }
+#imageInput{
+    opacity: 0;
+    width: 100px;
+}
+#select-image{
+    background-color: black;
+    color: white;
+    height: 40px;
+    border-radius: 10px;
+}
+
+#postImage{
+     margin-top: -30px;
+}
+.traco2{
+    border: 0.1px solid #989898;
+    float: right;
+    height: 35px;
+}
+
+.icon{
+    background-color: #68A35D;
+    color: white;
+    width: 40px;
+    text-align: center;
+    padding-top: 10px;
+    height: 40px;
+    border-radius: 50%;
+    margin-left: 10px;
+    margin-right: 5px;
+}
+
 #front, #verse{
     float: left;
     padding-right: 10px;
@@ -262,42 +330,31 @@ nav ul li a {
     background-color: #B28DD6;
     border-radius: 0 10px 10px 0;
 }
+.g{
+    border: 1px solid green;
+}
+.b{
+    border: 1px solid purple;
+}
 
-#select-image{
-    background-color: black;
-    color: white;
+#edit {
+    float: left;
     height: 40px;
-    width: 200px !important;
-    border-radius: 10px;
-    
-  
-}
-#postImage{
-     margin-top: -30px;
-}
-.shapes, .fields{ 
-    width: 280px !important;
     color:#989898;
-   
+    margin-right: 10px;
+    margin-top: 5px;
 }
-.shapes {
-    margin-left: -40px;
-}
-.fields {
-    margin-left: -10px;
+.flex {
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 1100px;
 }
 
-.icon{
-    background-color: #68A35D;
-    color: white;
-    width: 40px;
-    text-align: center;
-    padding-top: 10px;
-    height: 40px;
-    border-radius: 50%;
-    margin-left: 10px;
-    margin-right: 5px;
+.flex > div {
+  flex: 1 1 200px;
+  margin: 10px;
 }
+
 .iconPostImage{
     background-color: rgb(0, 0, 0);
     color: rgb(255, 255, 255);
@@ -305,38 +362,17 @@ nav ul li a {
     margin-left: 10px;
     
 }
-.traco{
-    border: 0.1px solid #989898;;
-    float: right;
-    height: 35px;
-    margin-right: 10px;
-}
-.traco2{
-    border: 0.1px solid #989898;;
-    float: right;
-    height: 35px;
-    margin-right: 30px;
-}
+/* CSS Grid 2 */
 
-.linha{
-    margin-top: 30px;
-    border-top: 1px dashed rgb(180, 177, 177);
-    position: relative;
-    width: 100%;
-}
-.underTheLine{
-    position: absolute;
-    background-color: black;
-    border-radius: 30px;
-    text-align: center;
-    margin-top: -10px;
-    color: white;
-    width: 90px;
-    margin-left: 10px;
-    text-transform: uppercase;
-}
-#myCanvas{
-    border: 1px solid rgb(146, 142, 142);
+.grid2 {
+    display: grid;
+    grid-template-columns: 1fr 200px;
+    grid-gap: 20px;
+    max-width: 1000px;
+    padding: 10px;
+  }
+  #myCanvas{
+     border: 1px solid rgb(146, 142, 142);
     width: 900px !important;
     height: 662px !important;
      background-color:  #F9F9F9;
@@ -344,44 +380,43 @@ nav ul li a {
     box-shadow:  0 5px 6px -6px black;
     margin-top: 30px;
     margin-left: 20px;
-}
-#imageInput{
-    opacity: 0;
-    width: 180px;
-}
 
+  }
+
+  .sidebar > div {
+    margin-bottom: 10px;
+    
+  }
+  
 .boxDimension{
     width: 150px;
     height: 80px;
     color: rgb(128, 128, 128);
-    padding-right: -50px;
-    padding-left: 10px;
     border: 1px solid rgb(223, 219, 219);
     border-radius: 3px;
     box-shadow:  0 5px 6px -6px rgb(97, 96, 96);
     font-size: 15px;
+    margin-top: 30px;
     margin-bottom: 20px;
+    padding-left: 10px;
+    margin-left: 20px;
 }
-.boxes{
-     margin-top: 30px;
-     flex-direction: column;;
-}
+
 .XW{
     float: left;
 }
 .YH{
     float: right;
 }
+
 .stroke{
     width: 180px;
     height: 137px;
     color: rgb(128, 128, 128);
-    padding-right: -50px;
     border: 1px solid rgb(223, 219, 219);
     border-radius: 3px;
     box-shadow:  0 5px 6px -6px rgb(97, 96, 96);
     font-size: 15px;
-    margin-left: -30px;
     
 }
 #strokeWord{
@@ -397,27 +432,137 @@ nav ul li a {
     margin-top: -5px;
     color: black;
 }
+.container{
+   display: flex;
+   flex-wrap: nowrap;  
+   
+}
+.item1{
+   width: 70%;  
+   border: 1px solid rgb(202, 197, 197) !important;
+   height: 30px;
+   border-radius: 5px;
+   margin-left: -10px;
+   margin-right: 10px;
+   
+}
+.item{
+    width: 30%;
 
-
-/* Extra small devices (phones, 600px and down) */
-@media (max-width: 400px) {
-#default-sidebar {
-    font-size: 12px;
-    width: 150px !important; 
+}
+#caixaColor{
+    width: 20px;
+    height: 20px;
+    background-color: black;
+    border: black;
+    margin-top: 5px;
+    margin-left: 5px;
+}
+#zero{
+    margin-left: 25px;
+    font-size: 13px;
+    color: black;
+}
+.eye{
+    margin-right: 10px;
+}
+.eye, .minus{
+    color: black;
 }
 
+.container2{
+    display: flex;
+   flex-wrap: nowrap;  
+    margin-top: 10px;
+    margin-left: 10px;
+}
+#bars{
+  width: 30%;  
+ 
+}
+#center{
+    width: 60%;
+    font-size: 13px;
+
+}
+.down{
+    font-size: 10x;
+    text-align: center;
+}
+.down2{
+    font-size: 25px;
+     color: black;
+}
+.item3{
+   width: 40%;  
+  
+}
+.item4{
+    justify-content: end;
 }
 
-/* Small devices (portrait tablets and large phones, 600px and up) */
-@media only screen and (min-width: 600px) {}
+.ellipsis{
+    color: black;
+}
 
-/* Medium devices (landscape tablets, 768px and up) */
-@media only screen and (min-width: 768px) {}
+  @media (max-width: 500px) {
+    .grid2 {
+      grid-template-columns: 2fr;
+    }
 
-/* Large devices (laptops/desktops, 992px and up) */
-@media only screen and (min-width: 992px) {}
+     #myCanvas{
+     width: 150px !important;
+     height: 150px !important;
+     margin-left: 5px;
+ }   
+  }
+ @media (min-width: 500px ) and (max-width: 600px) {
+      .grid2 {
+      grid-template-columns: 2fr;
+    }
+ #myCanvas{
+     width: 250px !important;
+     height: 200px !important;
+  }
+ }
+   @media (min-width: 600px ) and (max-width: 750px) {
+       .grid2 {
+      grid-template-columns: 2fr;
+    }
+ #myCanvas{
+     width: 300px !important;
+     height: 250px !important;
+     margin-left: 3px;
+  }
+  
+#front, #verse{
+    width: 50px;
 
-/* Extra large devices (large laptops and desktops, 1200px and up) */
-@media only screen and (min-width: 1200px) {}
+}
+ }
+ 
+   @media (min-width: 750px ) and (max-width: 900px) {
+      
+ #myCanvas{
+     width: 350px !important;
+     height: 300px !important;
+     margin-left: 3px;
+  }
+ }
+  @media (min-width: 900px ) and (max-width: 1100px) {
+ #myCanvas{
+     width: 450px !important;
+     height: 400px !important;
+  }
+ }
+ @media(min-width: 1100px) and (max-width: 1270px){
+      #myCanvas{
+     width: 600px !important;
+     height: 550px !important;
+  }
+#front, #verse{
+    width: 55px;
 
+}
+ }
 </style>
